6e82cb8680f0eabafbfc4f48dd198f5ca4a5610c
Emilia Yoffie <emiliayoffie@arcane-air.myfiosgateway.com>
2022-12-20 15:41:25 -0500

    updated mapping and tests

--- import/mapping.ts
+++ import/mapping.ts
@@ -1,10 +1,16 @@
 import * as readline from 'node:readline/promises';
 import { stdin, stdout } from 'node:process';
 import { sheets_v4 } from 'googleapis';
+import * as fs from 'node:fs';
+import { promises } from 'fs';
+import * as path from 'path'; 
+// import fs from '../__mocks__/fs';
 
-// export const createMap = (columns: Set<string>) => {
+const { readFile, writeFile } = promises;
 
-// };
+const createMap = (columns: Set<string>) => {
+
+};
 
 // type Stage = 'Mixpanel' | 'Spreadsheet' | 'Mapping';
 
@@ -13,23 +19,39 @@ type MixpanelCredentials = {
   SERVICE_ACCOUNT: string;
   SERVICE_ACCOUNT_PASSWORD: string;
 };
+
+const MIX_CRED = path.join(process.cwd(), '.mixpanel');
+
 const authorizeMixpanel = async (): Promise<MixpanelCredentials> => {
-  const input: { [key: string]: string } = {};
-  const rl = readline.createInterface(stdin, stdout);
-  const questions = {
-    PROJECT_ID: 'What is your Mixpanel Project ID?',
-    SERVICE_ACCOUNT: 'What is your Mixpanel Service Account Username?',
-    SERVICE_ACCOUNT_PASSWORD: 'What is your Mixpanel Service Account Password?',
-  } as MixpanelCredentials;
-  const keys = Object.keys(questions);
-  for (let i = 0; i < keys.length; i += 1) {
-    const key = keys[i];
-    // eslint-disable-next-line no-await-in-loop
-    input[key] = await rl.question(`${questions[key as keyof typeof questions]} \t`);
+  const mixpanel = './mixpanel';
+  if (fs.existsSync(mixpanel)) {
+    console.log('file exists')
+    const creds = await readFile(mixpanel);
+    const credentials = JSON.parse(creds.toString());
+    if (!Object.values(credentials).includes('')) { 
+      console.log('this is credentials', credentials);
+    }
+  } else {
+    const input: { [key: string]: string } = {};
+    const rl = readline.createInterface(stdin, stdout);
+    const questions = {
+      PROJECT_ID: 'What is your Mixpanel Project ID?',
+      SERVICE_ACCOUNT: 'What is your Mixpanel Service Account Username?',
+      SERVICE_ACCOUNT_PASSWORD: 'What is your Mixpanel Service Account Password?',
+    } as MixpanelCredentials;
+    const keys = Object.keys(questions);
+    for (let i = 0; i < keys.length; i += 1) {
+      const key = keys[i];
+      // eslint-disable-next-line no-await-in-loop
+      input[key] = await rl.question(`${questions[key as keyof typeof questions]} \t`);
+    }
+    rl.close();
+    await writeFile(MIX_CRED, JSON.stringify(input));
+    // console.log('this is input', input)
+    return input as typeof questions;
   }
-  rl.close();
-  return input as typeof questions;
 };
+authorizeMixpanel();
 
 const getSpreadsheet =
   async (): Promise<sheets_v4.Params$Resource$Spreadsheets$Values$Get> => {
@@ -46,18 +68,19 @@ const getSpreadsheet =
       input[key] = await rl.question(`${questions[key as keyof typeof questions]} \t`);
     }
     rl.close();
+    console.log(input)
     return input as typeof questions;
   };
-getSpreadsheet();
 
-
+// getSpreadsheet();
 // type MappingType = {
 //   distinct_id: string,
-//   eventName: string,
+  // eventName: string,
 //   $insert_id: string,
 //   timestamp: string,
 //   custom: string[],
 // };
+
 
 //   case 'Mapping':
 //     // questions = {
@@ -71,4 +94,4 @@ getSpreadsheet();
 //   }
 // }
 
-export { authorizeMixpanel, getSpreadsheet };
+export { authorizeMixpanel, createMap, getSpreadsheet };
