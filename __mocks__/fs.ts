4cdf81e992baf147777c2bb54a2bea0cafc90ce8
Matthew Lapeer <matthew.lapeer@gmail.com>
2022-12-20 17:08:22 -0500

    add comments to mocked modules and mocked module tests

--- __mocks__/fs.ts
+++ __mocks__/fs.ts
@@ -1,26 +1,36 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable @typescript-eslint/naming-convention */
+
+// Declare type for mocked fs module, needed to add properties to mocked module
 type FSMockType = typeof import('node:fs/promises') & { __setMockFiles: jest.Mock };
 
+// Mock the entire promises object from fs module
 const fs = jest.createMockFromModule<FSMockType>('node:fs/promises');
 
+// Declare mockFiles to act as file system storage, inherit from null to avoid prototype properties
 let mockFiles = Object.create(null);
 
-// eslint-disable-next-line @typescript-eslint/naming-convention, no-underscore-dangle
+// Define method for saving a mocked file system
 const __setMockFiles = (newMockFiles: { [key:string]: string }): void => {
+  // Reassign mocked file system storage, inherit from null to avoid prototype properties
   mockFiles = Object.create(null);
+
+  // Iterate through input object keys
   Object.keys(newMockFiles).forEach((filepath) => {
+    // Save data for specified key from input object to mocked file system storage
     mockFiles[filepath] = newMockFiles[filepath];
   });
 };
 
-// eslint-disable-next-line no-underscore-dangle
+// Save the __setMockFiles method on the exported (mocked) module
 fs.__setMockFiles = __setMockFiles as jest.Mock;
+
+// Mock readFile logic to read values from mocked file system
 fs.readFile = jest.fn().mockImplementation((filepath: string) => Promise.resolve(mockFiles[filepath] || ''));
-fs.writeFile = (
-  filepath: string,
-  data: string
-) => {
+
+// Mock writeFile logic to write values to mocked file system
+fs.writeFile = (filepath: string, data: string) => {
   mockFiles[filepath] = data;
-  // eslint-disable-next-line no-underscore-dangle
   return Promise.resolve();
 };
 
