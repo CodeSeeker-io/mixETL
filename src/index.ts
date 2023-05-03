9470954b26d9f2cb2c364f8873a194fb522c73f5
matthewlapeer <64608882+matthewlapeer@users.noreply.github.com>
2023-05-03 13:15:35 -0400

    completes index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,55 @@
 import { OAuth2Client } from 'google-auth-library';
-import { authorize, listRows } from './import';
-import { authorizeMixpanel, getSpreadsheet, createMap } from './digest/mapping';
-import mixpanelAuth from './export';
+import { authorize as authorizeGoolge, getRows } from './import/import';
+import {
+  createMap,
+  getSpreadsheet,
+} from './digestion/mapping';
+import digest from './digestion/digest';
+import {
+  authorizeMixpanel,
+  getMixpanelCredentials,
+  SuccessResponse,
+  ErrorResponse,
+} from './export/export';
 
-const main = () => (async function mixETL(): Promise<void> {
-  try {
+export default function main() {
+  return (async function mixETL(): Promise<SuccessResponse | ErrorResponse | Error> {
+    try {
     // Retrieve saved credentials or save Mixpanel credentials from user input
-    const mixpanelCredentials = await authorizeMixpanel();
+      const mixpanelCredentials = await getMixpanelCredentials();
 
-    // Authorize Google Sheets API
-    const googleAuth = await authorize() as OAuth2Client;
+      // Authorize Google Sheets API
+      const googleAuth = await authorizeGoolge() as OAuth2Client;
 
-    // Get the target speadsheet parameters from user input
-    const targetSheet = await getSpreadsheet();
+      // Get the target speadsheet parameters from user input
+      const targetSheet = await getSpreadsheet();
 
-    // Load the data from the target spreadsheet
-    const sourceData = await listRows(googleAuth, targetSheet);
+      // Load the data from the target spreadsheet
+      const sourceData = await getRows(googleAuth, targetSheet);
 
-    // Pass the header row of target spreadsheet to create mapping with user input
-    const map = await createMap(new Set(sourceData[0]));
+      // Pass the header row of target spreadsheet to create mapping with user input
+      const map = await createMap(new Set(sourceData[0]));
 
-    // Pass the map object and loaded spreadsheet data and digest prior to export
-    const digested = digest(map, sourceData);
+      // Pass the map object and loaded spreadsheet data to digest prior to export
+      const digested = digest(sourceData, map);
 
-    // Save the Mixpanel SDK instance  after adding the credentials to it
-    const mixpanelSDK = mixpanelAuth(mixpanelCredentials);
+      // Save the object exposing Mixpanel SDK instance after adding the credentials to it
+      const mixpanelSDK = authorizeMixpanel(mixpanelCredentials);
 
-    // Export the source data to the destination using Mixpanel SDK
-    const mixpanelResponse = await mixpanelSDK.exportData(digested);
+      // Export the source data to the destination using Mixpanel SDK
+      const mixpanelResponse = await mixpanelSDK.exportData(digested);
 
-    // Log the Mixpanel respnonse for the user
+      // Log the Mixpanel respnonse for the user
+      // eslint-disable-next-line no-console
+      console.log(mixpanelResponse);
+      return mixpanelResponse;
+    } catch (err) {
     // eslint-disable-next-line no-console
-    console.log(mixpanelResponse);
-  } catch (err) {
-    // eslint-disable-next-line no-console
-    console.error(err);
-  }
-}());
+      console.error(err);
 
-main();
+      return err;
+    }
+  }());
+}
+
+// main();
