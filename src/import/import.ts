a84412f9a061de2144096e05588e53b20567c997
matthewlapeer <64608882+matthewlapeer@users.noreply.github.com>
2023-05-10 17:40:36 -0400

    updates google credential file names and adds them to gitignore

--- src/import/import.ts
+++ src/import/import.ts
@@ -11,8 +11,8 @@ const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = path.join(process.cwd(), 'token.json');
-const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+const TOKEN_PATH = path.join(process.cwd(), '.sheetsToken');
+const CREDENTIALS_PATH = path.join(process.cwd(), '.sheets');
 
 /**
  * Reads previously authorized credentials from the save file.
